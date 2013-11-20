876caee81c89bdb77db4c3a8cbb6b40fc3dac991
yada-ITA <yada_keiko@ogis-ri.co.jp>
2013-11-20 15:03:47 +0900

    バリデート追加 ・user　メール:必須入力 ・engine シリアルNo:エンジン型式とセットにした場合にユニークになること

--- app/models/engine.rb
+++ app/models/engine.rb
@@ -2,5 +2,7 @@ class Engine < ActiveRecord::Base
 
   # Association
   belongs_to :enginestatus
-  
+  validates :serialno, :presence => true,
+                       :uniqueness =>{:scope => :engineModelName }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,6 @@ class User < ActiveRecord::Base
   
   validates :name, :presence => true
 
+  validates :email, :presence => true
 
 end
