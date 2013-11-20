634c44812308994981836788ff569ef8c2733e0a
yada-ITA <yada_keiko@ogis-ri.co.jp>
2013-11-20 15:28:38 +0900

    エンジン型式を必須

--- app/models/engine.rb
+++ app/models/engine.rb
@@ -2,6 +2,7 @@ class Engine < ActiveRecord::Base
 
   # Association
   belongs_to :enginestatus
+  validates :engineModelName, :presence => true  
   validates :serialno, :presence => true,
                        :uniqueness =>{:scope => :engineModelName }
 
