3d2bf2fd4623fab5322c024cbb548f1a10e81351
Kazutaka Sankai <Sankai_Kazutaka@ogis-ri.co.jp>
2013-11-20 15:00:41 +0900

    エンジンと会社を対応付け、エンジン一覧に検索機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119112049) do
+ActiveRecord::Schema.define(version: 20131120043857) do
 
   create_table "arrivals", force: true do |t|
     t.date     "arrivalDate"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20131119112049) do
     t.datetime "updated_at"
     t.integer  "enginestatus_id"
     t.string   "serialno"
+    t.integer  "company_id"
   end
 
   create_table "enginestatuses", force: true do |t|
