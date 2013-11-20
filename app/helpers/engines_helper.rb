caaf4f921eafff36e0ed743d3c9a71a4f2c84d41
yada-ITA <yada_keiko@ogis-ri.co.jp>
2013-11-20 17:36:18 +0900

    YES本社かそれ以外かの判断をヘルパークラスに追加 検索ボタン押下の結果でも、YES本社以外の場合は自社以外のエンジンが見えないように変更 エンジン情報登録・変更時にYES本社以外の場合は会社情報を変更できない（自社で登録される）ように変更

--- app/helpers/engines_helper.rb
+++ app/helpers/engines_helper.rb
@@ -1,5 +1,15 @@
+#coding: UTF-8
 module EnginesHelper
   def engine_arrived_path(engine)
     return new_repair_path + '/' + engine.id.to_s
   end
+  
+  def yesOffice?
+    if  Company.find(current_user.company_id).name == "YES本社"
+      return true      
+    else
+      return false
+    end
+  end
+  
 end
