99d0991f124d1ea00b2e809f6b965c17366c9c5d
yada-ITA <yada_keiko@ogis-ri.co.jp>
2013-11-28 13:40:25 +0900

    納期と送付先は、引合登録時も登録できるよう変更 device_ja.ymlの文言を一部変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  enginestatues = Enginestatus.create([{id: 1, name: '整備前'},  {id: 2,  name: '整備中'}, 
+   {id: 3,  name: '完成品'},  {id: 4,  name: '出荷準備中'},  {id: 5,  name: '出荷済'}, {id: 6,  name: '廃却済'}])
