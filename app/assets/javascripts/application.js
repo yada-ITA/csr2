b80b13173493ef3b47f0fba1a4b1e0dc3a78c5a6
Kazutaka Sankai <Sankai_Kazutaka@ogis-ri.co.jp>
2013-12-19 01:43:02 +0900

    オートコンプリート実装途中

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require autocomplete-rails
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require autocomplete-rails
+//= require_tree .
